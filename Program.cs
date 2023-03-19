7acb19485a2c4362994f2d55256c2542f1fa82b3
别听鬼故事 <btguigushi@qq.com>
2023-03-20 04:23:15 +0800

    refactor: 使用新的命令解析库

--- Program.cs
+++ Program.cs
@@ -1,39 +1,49 @@
-﻿using CommandLine;
+﻿using System.CommandLine;
 using SpeedCMD;
 
-CommandLine.Parser.Default.ParseArguments<ServerOptions, ClientOptions>(args)
-  .MapResult(
-    (ServerOptions opts) => RunServer(opts),
-    (ClientOptions opts) => RunClient(opts),
-    errs => 1);
+var rootCommand = new RootCommand("网络测速命令行工具");
+var serverCommand = new Command("server", "使用服务端");
+var clientCommand = new Command("client", "使用客户端");
 
-int RunServer(ServerOptions opts)
+rootCommand.AddCommand(serverCommand);
+rootCommand.AddCommand(clientCommand);
+
+// common options
+var portOption = new Option<int>("--port", "设置程序监听或访问的端口");
+portOption.AddAlias("-p");
+portOption.SetDefaultValue(22);
+portOption.AddValidator(result =>
 {
-  try
+  if (result.GetValueForOption(portOption) is not > 0 and < 65536)
   {
-    var server = new Server(opts.Port);
-    server.Start();
-    return 0;
+    result.ErrorMessage = "范围必须在[1-65535]之间";
   }
-  catch (System.Exception e)
-  {
-    System.Console.WriteLine("Exception: {0}", e);
-    return 1;
-  }
+});
+
+// server options
+//...
+
+// client options
+var hostOption = new Option<string>("--host", "设置远程主机或IP");
+hostOption.SetDefaultValue("127.0.0.1");
+
+serverCommand.AddOption(portOption);
+clientCommand.AddOption(portOption);
+clientCommand.AddOption(hostOption);
+
+serverCommand.SetHandler(DoServerCommand, portOption);
+clientCommand.SetHandler(DoClientCommand, portOption, hostOption);
+
+return await rootCommand.InvokeAsync(args);
+
+void DoServerCommand(int port)
+{
+  var server = new Server(port);
+  server.Start();
 }
 
-int RunClient(ClientOptions opts)
+void DoClientCommand(int port, string host)
 {
-  try
-  {
-    var client = new Client(opts.Port, opts.Host);
-    client.Connect();
-    return 0;
-  }
-  catch (System.Exception e)
-  {
-    System.Console.WriteLine("Exception: {0}", e);
-    return 1;
-  }
+  var client = new Client(port, host);
+  client.Connect();
 }
-
